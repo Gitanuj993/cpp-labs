1c8ea1f0f5de2a5d49206f7e92584fb004ecabe9
Gitanuj993 <anujtawar42@gmail.com>
2026-03-14 08:59:59 +0530

    Introduction to data structure

---
+++ DS/DS_lab01.cpp
@@ -0,0 +1,58 @@
+// AIM    :     Getting started with Data Structures
+
+/*
+    what is Datta Structure ?    :    Arrangement of data by which performance and effieciency increases.  keywords : {    Arrangement , efficient , performance }
+    
+    which type of arrangement ? :    
+        - Organizing data 
+        - Storing data 
+        - Managing data
+        - Retriving data
+Computer  or machine need a structured data or instruction to perform operations.        
+        
+Note : Different data structures have different rules , algorithm and implimentation.
+with different applications and use cases.
+     
+*/
+
+/*
+    Primitive data types  : provided by the programming language
+
+        1. int   - integer type value - 4 bytes or 32 bits       
+        eg.  int n1 = 34 ;
+        
+        2. float  - floatig values ( Decimals type ) -  4 bytes 
+        eg. float rate = 34.34 ;
+        
+        3. double  - floating decimal type -  8 bytes or 64 bits
+        eg. double pi =   3.14372 ;
+        
+        4. long    -  integer type - 8 bytes  
+        eg . long int population = 60000000 ;
+        
+        5. char     - char stands for characters. -   1 byte 
+        eg . char c1 = 'A' ; char c2 = '3' ;                
+*/
+
+/*
+    Linear data structures !
+    
+    1. Array :   Books placed in a book shelf .
+    2. Stack :    stack of plates  , Last come First served .
+    3. Queue :    line for ticket for a movie , First in First served .
+    4. Linked-list :    Train coaches connect one another .
+    
+    Non Linear Data Structure !
+    
+    1.  Tree :    like Family Hierarchy
+    2. Graph :   Map using cities and road .
+    
+*/
+
+#include <iostream>
+using namespace std ;
+int main(int argc, char *argv[])
+{
+	cout <<  " Welcome AT \n" ;
+	return 0 ;
+}
